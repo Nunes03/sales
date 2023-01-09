f882c81575eb3adc3e70d46a34dd202725fa469c
Lucas Nunes <lucasnunes.ln365@gmail.com>
2023-01-09 19:58:31 -0300

    Corrigindo git ignore

--- src/main/java/io/github/nunes03/SalesApplication.java
+++ src/main/java/io/github/nunes03/SalesApplication.java
@@ -11,6 +11,7 @@ public class SalesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
+        System.out.println("teste");
         helloWorld();
     }
 
