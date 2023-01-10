d9c0dd0eb64362c5e05dd79cbd46493ed69ad1fa
Lucas Nunes <lucasnunes.ln365@gmail.com>
2023-01-10 20:00:08 -0300

    Conhecendo os Profiles com configurations e .properties

--- src/main/java/io/github/nunes03/settings/Settings.java
+++ src/main/java/io/github/nunes03/settings/Settings.java
@@ -1,13 +1,18 @@
 package io.github.nunes03.settings;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class Settings {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sales system.";
+    @Bean
+    public CommandLineRunner execute() {
+        return args -> {
+            System.err.println("Runnn");
+        };
     }
 }
