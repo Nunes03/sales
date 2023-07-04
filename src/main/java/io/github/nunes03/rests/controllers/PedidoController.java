08185caf96ddb130201eb2e6868a67764228d7e1
Lucas Nunes <lucasnunes.ln365@gmail.com>
2023-07-03 21:28:21 -0300

    Criando interface de métodos rest padrões

---
+++ src/main/java/io/github/nunes03/rests/controllers/PedidoController.java
@@ -0,0 +1,2 @@
+package io.github.nunes03.rests.controllers;public class PedidoController {
+}
---
+++ src/main/java/io/github/nunes03/rests/controllers/interfaces/RestControllerInterface.java
@@ -0,0 +1,2 @@
+package io.github.nunes03.rests.controllers.interfaces;public interface RestControllerInterface {
+}
